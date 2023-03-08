2d4cd1b9d1f68a07f18465289640de5615c5774a
rem beuch <beucherie.r@gmail.com>
2023-03-08 19:09:31 +0100

    delete zeid

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Dice421
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoloader = :zeitwerk
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
