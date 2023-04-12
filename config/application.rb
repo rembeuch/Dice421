76aef18c484e2c280fd4b6b0bc827a5dedc57065
rem beuch <beucherie.r@gmail.com>
2023-04-12 10:31:29 +0200

    setup

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Dice421
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoloader = :zeitwerk
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
